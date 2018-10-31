05e3dff42ce1b3fccba1846e5160725e1fb3c342
Shawn Camner <Shawn@camnersoftworks.com>
2018-10-31 09:50:15 -0400

    Check in

--- EmailDisableDistroGroup/RunWorkflowItem.cs
+++ EmailDisableDistroGroup/RunWorkflowItem.cs
@@ -27,23 +27,12 @@ namespace ItemToRun
 
                 Utilities util = new Utilities();
 
-                Employee manager = db.Employees.Where(e => e.EmpID == emp.ReportsTo).FirstOrDefault();
-
-                if (manager == null)
-                {
-                    throw new Exception(String.Format("There is no manager assigned to {0} {1}.", emp.FirstName, emp.LastName));
-                }
-
-                if (String.IsNullOrEmpty(manager.Email))
-                {
-                    throw new Exception(String.Format("The manager assigned to {0} {1} does not have an email set.", emp.FirstName, emp.LastName));
-                }
-
+                String disableGroup = util.GetParam("DisableDistroGroupEmail", "Disable Distro Group email address");
                 String emailServer = util.GetParam("EmailServer", "smtp (email) server name");
                 String from = util.GetParam("EmailFrom", "email address to send from");
-                String to = manager.Email;
-                String body = util.GetParam("DisabledNotifyBody", "message to managers to notify them of a disabled employee");
-                String subject = util.GetParam("DisabledNotifySubject", "subject line for the email to send managers to notify them of a disabled employee");
+                String to = disableGroup;
+                String body = util.GetParam("DisableDistroGroupBody", "message to disable distro group to notify them of a disabled employee");
+                String subject = util.GetParam("DisableDistroGroupSubject", "subject line for the email to send the disable distro group to notify them of a disabled employee");
 
                 foreach (PropertyInfo prop in typeof(Employee).GetProperties())
                 {
@@ -53,12 +42,27 @@ namespace ItemToRun
 
                 util.SendEmail(from, to, null, null, subject, body);
 
-                return new ItemRunResult { ResultID = 2, ResultText = String.Format("The email was sent to {0} {1}.", manager.FirstName, manager.LastName), TimeDone = DateTime.Now, RunPayload = jsonPL };
+                return new ItemRunResult { ResultID = 2, ResultText = String.Format("The email was sent to '{0}' regarding {1} {2}.", disableGroup, emp.FirstName, emp.LastName), TimeDone = DateTime.Now, RunPayload = jsonPL };
             }
             catch (Exception ex)
             {
                 return new ItemRunResult { ResultID = 4, ResultText = String.Format("Error: {0}", ex.Message), TimeDone = DateTime.Now, RunPayload = jsonPL };
             }
         }
+    }
+
+    public class ItemRunResult
+    {
+        public int ResultID { get; set; }
+        public String ResultText { get; set; }
+        public DateTime TimeDone { get; set; }
+        public String RunPayload { get; set; }
+    }
+
+    public class RunPayloadModel
+    {
+        public String TargetLibraryName { get; set; }
+        public String PayloadParent { get; set; }
+        public String TargetPayload { get; set; }
     }
 }
